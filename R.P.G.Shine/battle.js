802d79148eed6b30603c2fec240ce4a12fb729c3
guswndeks <114652359+guswndeks@users.noreply.github.com>
2023-12-12 11:48:45 +0900

    23.12.12 R.P.G.Shine 중간 작업(중단)
     서로 데이터를 주고 받는 작동을 테스트 하는 것을 목표로 작업했습니다.
     battle.js에 있는 코드들을 모두 수정했지만, 최종적으로 import하는 과정에서 CORS 정책 위반으로 인해 중단되었습니다.
     해당 문제의 원인은 제대로 된 구조로 진행하지 않고 JS에서 동적인 부분들을 무리하게 만들어서 발생한 것으로 보입니다.
     이에 따라 제대로 된 동적 파트 및 서버를 연결하여 제대로 된 구조를 제작할 여건이 될 때 다시 작업에 들어갈 계획입니다.
     당분간 R.P.G.Shine 프로젝트는 중단합니다.

--- R.P.G.Shine/battle.js
+++ R.P.G.Shine/battle.js
@@ -1,4 +1,9 @@
+let M; // M 변수를 밖에서 정의
+
+
 document.addEventListener("DOMContentLoaded", function() {
+
+
     // URL에서 MapNumber 값을 가져오는 함수
     function getParameterByName(name, url) {
         if (!url) url = window.location.href;
@@ -15,37 +20,42 @@ document.addEventListener("DOMContentLoaded", function() {
     
     // 가져온 값 확인 (콘솔에 출력)
     console.log("MapNumber:", mapNumber);
-    
-    // 이후에 필요한 작업을 여기에 추가할 수 있습니다.
 
-if (mapNumber == 1) {
-    import('./main.js').then(module => {
-        const SvFrontMonster = module.SvFrontMonster;
+    if (mapNumber == 1) {
+        import('./main.js').then(module => {
+            const SvFrontMonster = module.SvFrontMonster;
 
-        const M = getMonster();
-        let matchedInstance;
+            M = getMonster(); // M 값을 설정
 
-        // SvFrontMonster 인스턴스를 생성하고 label 값이 M과 일치하는지 확인하는 함수
-        function createAndFindMatchingInstance(labelValue) {
-            const instance = new SvFrontMonster(labelValue);
-            if (instance.label === M) {
-                matchedInstance = instance;
+            let matchedInstance;
+
+            // SvFrontMonster 인스턴스를 생성하고 label 값이 M과 일치하는지 확인하는 함수
+            function createAndFindMatchingInstance(labelValue) {
+                const instance = new SvFrontMonster(labelValue);
+                if (instance.label === M) {
+                    matchedInstance = instance;
+                }
             }
-        }
 
-        // 여러 인스턴스를 생성하고 일치하는 값을 찾을 수 있도록 반복문 사용
-        for (let i = 0; i < 5; i++) { // 예시로 10개의 인스턴스 생성
-            createAndFindMatchingInstance(i); // 예시로 인스턴스에 순차적인 값을 넣음
-        }
+            // 여러 인스턴스를 생성하고 일치하는 값을 찾을 수 있도록 반복문 사용
+            for (let i = 0; i < 5; i++) { // 예시로 10개의 인스턴스 생성
+                createAndFindMatchingInstance(i); // 예시로 인스턴스에 순차적인 값을 넣음
+            }
 
-        if (matchedInstance) {
-            console.log('Matched label:', matchedInstance.label);
-            // 여기서 matchedInstance에 해당하는 작업을 수행합니다.
-        } else {
-            console.log('No match found.');
-        }
-    });
-}
+            if (matchedInstance) {
+                console.log('Matched label:', matchedInstance.label);
+                // 여기서 matchedInstance에 해당하는 작업을 수행합니다.
+            } else {
+                console.log('No match found.');
+            }
+
+            console.log(M); // M 값을 콘솔에 출력
+            // M 값을 HTML에 추가하기 위한 코드
+            var myDiv = document.createElement('div');
+            myDiv.textContent = M;
+            document.body.appendChild(myDiv);
+        });
+    }
 
 
 // 특정 확률에 따라 1,2,3 중 하나를 뽑아서 위 함수 중 M 변수로 보냄.(해당 숫자는 이후 main.js/~Monster 내 label과 비교, 동일 label을 가진 인스턴스를 가져옴.)
@@ -64,18 +74,24 @@ function getMonster()
     return coin;
 
 }
-
-
-
-
-
-
-while(true){
-	proc_battle();
-
-	if(elf.hp <= 0 || dragon.hp <= 0){
-		break;
-	}
-}
+    var myDiv = document.createElement('div'); // 새로운 div 요소 생성
+    myDiv.textContent = M; // div에 M 값을 추가
+    document.body.appendChild(myDiv); // body에 해당 div를 추가
 
 });
+
+
+
+
+
+
+
+
+// while(true){
+// 	proc_battle();
+
+// 	if(elf.hp <= 0 || dragon.hp <= 0){
+// 		break;
+// 	}
+// }
+
